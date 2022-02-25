363c5ed82461a5dffb3408de2c9918c61121984a
KrisKarlsson <kris.e.h.karlsson@gmail.com>
2022-02-25 14:32:31 +1100

    - Set up project

---
+++ LearningApp/ViewModel/ContentModel.swift
@@ -0,0 +1,16 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Kris Karlsson on 25/2/2022.
+//
+
+import Foundation
+
+
+class ContentModel: ObservableObject{
+    
+    
+    
+    
+}
