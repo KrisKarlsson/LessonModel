fcf0e77249f97111cd0e15e030427a0d956743ac
KrisKarlsson <kris.e.h.karlsson@gmail.com>
2022-02-25 14:22:00 +1100

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Kris Karlsson on 25/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
